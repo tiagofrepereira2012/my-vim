3e1c9f417bcaf2180272f5f8b2833adebb3dd5d8
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2019-11-11 16:07:27 +0100

    Update setup-my-vim.sh

--- setup-my-vim.sh
+++ setup-my-vim.sh
@@ -6,5 +6,6 @@ cp -R .vim ~
 git clone https://github.com/VundleVim/Vundle.vim
 mv Vundle.vim ~/.vim/bundle
 vim +PluginUpdate +qall
-pip install black --user
+wget https://github.com/snakemake/snakemake/raw/master/misc/vim/syntax/snakemake.vim
+mv snakemake.vim ~/.vim/
 
